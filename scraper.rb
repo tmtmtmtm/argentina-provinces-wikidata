c65a03cbce37b41bb4753bbecca9ca5b776fc653
Tony Bowden <tony@tmtm.com>
2017-09-27 15:43:47 +0100

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,19 @@
+#!/bin/env ruby
+# encoding: utf-8
+# frozen_string_literal: true
+
+require 'pry'
+require 'scraperwiki'
+require 'wikidata/fetcher'
+require 'wikidata/area'
+
+query = <<-EOQ
+  SELECT DISTINCT ?item WHERE { ?item wdt:P31 wd:Q44753 .  }
+EOQ
+
+ids = EveryPolitician::Wikidata.sparql(query)
+raise 'No ids' if ids.empty?
+
+ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
+data = Wikidata::Areas.new(ids: ids).data
+ScraperWiki.save_sqlite(%i(id), data)
